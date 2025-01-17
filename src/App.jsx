454783444ada67a832178ca2f8274ebca4c72bd3
Olesia Vysotina <vysotina2002@gmail.com>
2025-01-17 21:55:23 +0100

    created project's root

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,42 @@
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+import { lazy } from 'react';
+// import { Suspense } from 'react';
 
-function App() {}
+// import Loader from './components/Loader/Loader';
+// import Navigation from './components/Navigation/Navigation';
+
+const HomePage = lazy(() => import('./pages/HomePage/HomePage'));
+// const CatalogPage = lazy(() => import('./pages/CatalogPage/CatalogPage'));
+// const CatalogDetailsPage = lazy(() =>
+//   import('./pages/CatalogDetailsPage/CatalogDetailsPage')
+// );
+// const CatalogFeature = lazy(() =>
+//   import('./components/CatalogFeature/CatalogFeature')
+// );
+// const CatalogReviews = lazy(() =>
+//   import('./components/CatalogReviews/CatalogReviews')
+// );
+// const NotFoundPage = lazy(() => import('./pages/NotFoundPage/NotFoundPage'));
+
+const App = () => {
+  return (
+    <>
+      <Toaster position="top-right" />
+      {/* <Navigation />
+      <Suspense fallback={<Loader />}> */}
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        {/* <Route path="/catalog" element={<CatalogPage />} />
+          <Route path="/catalog/:id" element={<CatalogDetailsPage />}>
+            <Route path="feature" element={<CatalogFeature />} />
+            <Route path="reviews" element={<CatalogReviews />} />
+          </Route>
+          <Route path="*" element={<NotFoundPage />} /> */}
+      </Routes>
+      {/* </Suspense> */}
+    </>
+  );
+};
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -4,18 +4,18 @@ import App from './App.jsx';
 import './index.css';
 import 'modern-normalize';
 import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-import { store, persistor } from './redux/store';
+// import { Provider } from 'react-redux';
+// import { PersistGate } from 'redux-persist/integration/react';
+// import { store, persistor } from './redux/store';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </PersistGate>
-    </Provider>
+    {/* <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}> */}
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+    {/* </PersistGate>
+    </Provider> */}
   </StrictMode>
 );
