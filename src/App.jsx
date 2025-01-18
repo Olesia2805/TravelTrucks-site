e0507cbc3d90d8ce839a04c66c8966c73129f32c
Olesia Vysotina <vysotina2002@gmail.com>
2025-01-18 23:01:17 +0100

    Implement lazy loading for CatalogPage with Suspense and Loader fallback

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
-import { lazy } from 'react';
-// import { Suspense } from 'react';
+import { lazy, Suspense } from 'react';
 
-// import Loader from './components/Loader/Loader';
+import Loader from './components/Loader/Loader';
 // import Navigation from './components/Navigation/Navigation';
 
 const HomePage = lazy(() => import('./pages/HomePage/HomePage'));
-// const CatalogPage = lazy(() => import('./pages/CatalogPage/CatalogPage'));
+const CatalogPage = lazy(() => import('./pages/CatalogPage/CatalogPage'));
 // const CatalogDetailsPage = lazy(() =>
 //   import('./pages/CatalogDetailsPage/CatalogDetailsPage')
 // );
@@ -23,18 +22,18 @@ const App = () => {
   return (
     <>
       <Toaster position="top-right" />
-      {/* <Navigation />
-      <Suspense fallback={<Loader />}> */}
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        {/* <Route path="/catalog" element={<CatalogPage />} />
-          <Route path="/catalog/:id" element={<CatalogDetailsPage />}>
-            <Route path="feature" element={<CatalogFeature />} />
-            <Route path="reviews" element={<CatalogReviews />} />
-          </Route>
-          <Route path="*" element={<NotFoundPage />} /> */}
-      </Routes>
-      {/* </Suspense> */}
+      {/* <Navigation /> */}
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/catalog" element={<CatalogPage />} />
+          {/* <Route path="/catalog/:id" element={<CatalogDetailsPage />}>
+          <Route path="feature" element={<CatalogFeature />} />
+          <Route path="reviews" element={<CatalogReviews />} />
+        </Route> */}
+          {/* <Route path="*" element={<NotFoundPage />} /> */}
+        </Routes>
+      </Suspense>
     </>
   );
 };
